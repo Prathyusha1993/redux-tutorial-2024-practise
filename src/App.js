63e70a58e37cf4a2c4f2eb4b4aaee0e9cd1ba085
Prathyusha Kurigala <prathyushakiran@Prathyushas-MBP.attlocal.net>
2024-06-04 18:59:44 -0700

    post slice using redux

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import "./App.css";
+import PostsList from "./features/posts/PostsList";
 // import Counter from "./features/counter/Counter";
 
 function App() {
@@ -6,6 +7,7 @@ function App() {
     <div className="App">
       {/* <h4>Counter App</h4>
       <Counter /> */}
+      <PostsList />
     </div>
   );
 }
--- src/reduxApp/store.js
+++ src/reduxApp/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
+import postSlice from "../features/posts/postSlice";
 // import counterSlice from "../features/counter/counterSlice";
 
 export const store = configureStore({
   reducer: {
     // counter: counterSlice,
+    posts: postSlice,
   },
 });
